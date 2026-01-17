c660319af1b7c7a2f8fdd5857d713be53ea9fa6f
codexr941 <xianze@ucsb.edu>
2026-01-16 20:41:24 -0800

    Implement copy constructor, destructor, and list methods

--- intlist.cpp
+++ intlist.cpp
@@ -9,12 +9,34 @@ using std::cout;
 
 // copy constructor
 IntList::IntList(const IntList& source) {
-    //IMPLEMENT THIS
+    head=nullptr;
+    if(source.head==nullptr)
+        return;
+    head=new Node;
+    head->info=source.head->info;
+    head->next=nullptr;
+    
+    Node *S=source.head->next;
+    Node *D=head;
+    while (S!= nullptr){
+        Node *N=new Node;
+        N->info=S->info;
+        N->next=nullptr；
+        D->next=N;
+        D=D->next;
+        S=S->next;
+    }
 }
 
 // destructor deletes all nodes
 IntList::~IntList() {
-    //IMPLEMENT THIS
+     Node* cur = head;
+    while (cur != nullptr) {
+        Node* nextNode = cur->next;
+        delete cur;
+        cur = nextNode;
+    }
+    head = nullptr;
 }
 
 
@@ -58,37 +80,89 @@ int IntList::max() const {
 // returns average (arithmetic mean) of all values, or
 // 0 if list is empty
 double IntList::average() const {
-    return 0.0; // REPLACE THIS NON-SOLUTION
+    return (double)sum()/count();
 }
 
 // inserts value as new node at beginning of list
 void IntList::push_front(int value) {
-    // IMPLEMENT
+    Node *n=head;
+    Node *m= new Node;
+    m->info=value;
+    head=m;
+    m->next=n;
 }
 
 // append value at end of list
 void IntList::push_back(int value) {
-    // IMPLEMENT
- 
+    Node *n=new Node;
+    n->info=value;
+    n->next=nullptr;
+    Node *t=head;
+    if (head == nullptr) {
+    head = n;
+    return;
+    }
+    while (t->next != nullptr){
+        t=t->next;
+    }
+    t->next=n;
 }
 
 // return count of values
 int IntList::count() const {
-   //IMPLEMENT THIS
-   return 0;
+  Node *n=head;
+    int count=0;
+    while (n!= nullptr){
+        count=count+1;
+        n=n->next;
+    }
+     return count;   
 }
 
 
 //Assignment operator should copy the list from the source
 //to this list, deleting/replacing any existing nodes
 IntList& IntList::operator=(const IntList& source){
-    //IMPLEMENT
+ if (this == &source) {
+        return *this;
+    }
+    Node* cur = head;
+    while (cur != nullptr) {
+        Node* nextNode = cur->next;
+        delete cur;
+        cur = nextNode;
+    }
+    head = nullptr;
+
+    if (source.head == nullptr) {
+        return *this;
+    }
+
+
+    head = new Node;
+    head->info = source.head->info;
+    head->next = nullptr;
+
+
+    Node* dest = head;
+    Node* src = source.head->next;
+
+    while (src != nullptr) {
+        dest->next = new Node;
+        dest = dest->next;
+
+        dest->info = src->info;
+        dest->next = nullptr;
+
+        src = src->next;
+    }
+
     return *this;
 }
 
 // constructor sets up empty list
 IntList::IntList(){ 
-    //IMPLEMENT THIS 
+  head=nullptr;
 }
 
 
