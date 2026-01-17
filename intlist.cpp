632b48a293ce5ebd455839f97a067ed8c3c2ecd1
codexr941 <xianze@ucsb.edu>
2026-01-16 23:15:56 -0800

    Fix syntax error in average function

--- intlist.cpp
+++ intlist.cpp
@@ -81,7 +81,7 @@ int IntList::max() const {
 // 0 if list is empty
 double IntList::average() const {
     if (count()==0)
-        return 0.0
+        return 0.0;
     return (double)sum()/count();
 }
 
